75a6bf97cf244efc8716e55a229194aff577b200
RobetLxx <847587959@qq.com>
2021-07-14 11:31:55 +0800

    Revert "去除不必要的引入"
    This reverts commit 02dfc36e3e7856a8289f392eaf81ba08631afb9a.

--- cron-demo/src/main.js
+++ cron-demo/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Antd from 'ant-design-vue';
 import App from './App';
 import 'ant-design-vue/dist/antd.css'
+import VueRouter from 'vue-router'
 import router from "./router";
 
 Vue.config.productionTip = false;
