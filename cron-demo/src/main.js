5c7aa9661206d6f51ce3c5cc28c1d63529f9975a
lingjunhao <847587959@qq.com>
2021-07-14 11:46:44 +0800

    去除不必要的依赖引入

--- cron-demo/src/main.js
+++ cron-demo/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Antd from 'ant-design-vue';
 import App from './App';
 import 'ant-design-vue/dist/antd.css'
-import VueRouter from 'vue-router'
 import router from "./router";
 
 Vue.config.productionTip = false;
